0c975c61404437076a2981d30ba277f201a768bb
Dmitry Valiukh <dmitry.valiukh@deplabs.com>
2020-07-22 23:57:54 +0300

    add linter

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,10 +6,10 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     }).compileComponents();
   }));
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-first-project'`, () => {
+  it("should have as title 'my-first-project'", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('my-first-project');
